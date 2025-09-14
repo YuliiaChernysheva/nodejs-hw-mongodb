4ba646f1fca976aa933a200e4c306b8a4acad5dd
Yuliia <chernyshevaulia2905@gmail.com>
2025-09-14 15:39:58 -0600

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import 'dotenv/config';
-import { setupServer } from './server.js';
-import { initMongoConnection } from './db/initMongoConnection.js';
+// import 'dotenv/config';
+// import { setupServer } from './server.js';
+// import { initMongoConnection } from './db/initMongoConnection.js';
 
-async function bootstrap() {
-  try {
-    await initMongoConnection();
-    setupServer();
-  } catch (error) {
-    console.error('App launch error:', error.message);
-  }
-}
+// async function bootstrap() {
+//   try {
+//     await initMongoConnection();
+//     setupServer();
+//   } catch (error) {
+//     console.error('App launch error:', error.message);
+//   }
+// }
+
+// bootstrap();
+
+import { initMongoConnection } from './db/initMongoConnection.js';
+import { setupServer } from './server.js';
+
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
 
 bootstrap();
