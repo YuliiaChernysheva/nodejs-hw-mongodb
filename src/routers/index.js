83c3aa7cf29485e3992d49a71ad89c1397321204
Yuliia <chernyshevaulia2905@gmail.com>
2025-10-09 14:19:23 -0600

    update moddlewaews/auth.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import authRouter from './auth.js';
 import contactsRouter from './contacts.js';
+import { auth } from '../middlewares/auth.js';
 
 const router = Router();
 
-router.use('/contacts', contactsRouter);
+router.use('/contacts', auth, contactsRouter);
 router.use('/auth', authRouter);
 
 export default router;
