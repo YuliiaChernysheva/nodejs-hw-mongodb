837f37690c813c66f573fec550be871bc5d88bfc
Yuliia <chernyshevaulia2905@gmail.com>
2025-10-22 15:37:44 -0600

    update swagger

--- src/server.js
+++ src/server.js
@@ -21,12 +21,12 @@ export function setupServer() {
 
   app.use('/uploads', express.static(UPLOAD_DIR));
 
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
+
   app.use(Router);
   app.use(notFoundHandler);
   app.use(errorHandler);
-
-  app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
